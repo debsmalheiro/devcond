a3d0735035849b69a10c524195ab920515e0e1ac
Déborah Malheiro <deblima_@hotmail.com>
2021-04-16 09:44:20 -0300

    Montando a API

---
+++ src/services/api.js
@@ -0,0 +1,51 @@
+const baseUrl = "https://api.b7web.com.br/devcond/api/admin";
+
+const request = async (method, endpoint, params, token = null) => {
+  method = method.toLowerCase();
+  let fullUrl = `${baseUrl}${endpoint}`;
+  let body = null;
+
+  switch (method) {
+    case "get":
+      let queryString = new URLSearchParams(params).toString();
+      fullUrl += `?${queryString}`;
+      break;
+    case "post":
+    case "put":
+    case "delete":
+      body = JSON.stringify(params);
+      break;
+  }
+  // Fazendo a requisição
+
+  let headers = { "Content-Type": "application/json" };
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+  let req = await fetch(fullUrl, { method, headers, body });
+
+  // Receber a requisição e transformar em JSON
+
+  let json = await req.json();
+  return json;
+};
+
+export default () => {
+  return {
+    // Pegar o token
+    getToken: () => {
+      return localStorage.getItem("token");
+    },
+    // Validar o token
+    validateToken: async () => {
+      let token = localStorage.getItem("token");
+      let json = await request("post", "/auth/validate", {}, token);
+      return json;
+    },
+    // Fazer o login
+    login: async (email, password) => {
+      let json = await request("post", "/auth/login", { email, password });
+      return json;
+    },
+  };
+};
